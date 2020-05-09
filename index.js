930066ebffcfd5eaf1dced4973968a1a7b02d1dc
Neuber Oliveira <neuber.oliveira05@gmail.com>
2020-05-09 20:22:05 -0300

    ....

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { app, screen, ipcMain } = require('electron')
 const store = require('./store')
 const terminalWindow = require('./windows/terminalWindow')
-const { sendLoader } = require('./trick-sender')
-const codeOutTrick = require('./tricks/code-out')
 const preparingTrick = require('./tricks/preparing')
 const bruteForceTrick = require('./tricks/brute-force')
 const openImage = require('./tricks/open-image')
@@ -15,11 +13,10 @@ app.whenReady().then(() => {
 	}
 
 	store.mainWindow = terminalWindow()
-	ipcMain.on('first-cmd', async (evt, cmd) => {
-		await preparingTrick()
-		await bruteForceTrick()
+	const mainWindowBounds = store.mainWindow.getBounds()
 
-		const mainWindowBounds = store.mainWindow.getBounds()
+
+	ipcMain.on('first-cmd', async (evt, cmd) => {
 		playVideo('../assets/video/MI66opu41Ck.mp4', {
 			width: 356,
 			height: 200,
@@ -31,12 +28,24 @@ app.whenReady().then(() => {
 			}
 		})
 
-		const imagePositionFactor = 60
-		const firstWinX = (store.screen.width - 600) - (imagePositionFactor * 2)
-		openImage('./assets/image/usa-presidential-seal.png', { x: firstWinX, y: 100, width: 600, height: 600, delay: 0, autoCloseDelay: 2000 })
-		openImage('./assets/image/usa-presidential-seal.png', { x: firstWinX + (imagePositionFactor), y: 100 + (imagePositionFactor), width: 600, height: 600, delay: 200, autoCloseDelay: 2000 })
-		openImage('./assets/image/top-secret.jpg', { x: firstWinX + (imagePositionFactor * 2), y: 100 + (imagePositionFactor * 2), width: 464, height: 600, delay: 400, autoCloseDelay: 2000 })
+		await preparingTrick()
+		await bruteForceTrick()
 
+		playVideo('../assets/video/p1KWW6hHQeg.mp4', {
+			width: 320,
+			height: 180,
+			x: store.screen.width - 260,
+			y: 160,
+			onEnd: (win) => {
+				win.close()
+				win.destroy()
 
+				const imagePositionFactor = 60
+				const firstWinX = (store.screen.width - 600) - (imagePositionFactor * 2)
+				openImage('./assets/image/usa-presidential-seal.png', { x: firstWinX, y: 100, width: 600, height: 600, delay: 0, autoCloseDelay: 2000 })
+				openImage('./assets/image/usa-presidential-seal.png', { x: firstWinX + (imagePositionFactor), y: 100 + (imagePositionFactor), width: 600, height: 600, delay: 200, autoCloseDelay: 2000 })
+				openImage('./assets/image/top-secret.jpg', { x: firstWinX + (imagePositionFactor * 2), y: 100 + (imagePositionFactor * 2), width: 464, height: 600, delay: 400, autoCloseDelay: 2000 })
+			}
+		})
 	})
 })
